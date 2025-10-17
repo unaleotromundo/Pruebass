87c802ca7f3f7a20bbe9b046ab3f3a801b51d4d1
unaleotromundo <unaleotromundo@gmail.com>
2025-10-17 00:57:08 -0300

    asd

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{ padding: '50px', backgroundColor: 'blue', color: 'white', fontSize: '30px' }}>
+      ¡FUNCIONA CON TSX!
+    </div>
+  );
+}
